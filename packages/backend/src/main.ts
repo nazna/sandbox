97caa393c0f5b59ff8073b19b06f6fd7d75f03fd
nazna <14846330+nazna@users.noreply.github.com>
2020-09-20 03:24:42 +0900

    refactor: change database usage

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -2,7 +2,12 @@ import compression from 'compression'
 import config from 'config'
 import express, { Express } from 'express'
 import helmet from 'helmet'
+import lowdb from 'lowdb'
+import FileSync from 'lowdb/adapters/FileSync'
 import books from './books'
+
+const adapter = new FileSync('src/db.json')
+export const db = lowdb(adapter)
 
 const { version } = config.get('backend')
 
