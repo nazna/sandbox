d47ecc997529989a05af0e33baf3ddcf111bec0a
nazna <14846330+nazna@users.noreply.github.com>
2020-09-22 23:35:25 +0900

    feat: ready to develop frontend

---
+++ packages/frontend/README.md
@@ -0,0 +1,7 @@
+# frontend
+
+> Frontend with Next.js
+
+## Todos
+
+- [ ] 単体テストを書く [testing-library/react-testing-library](https://github.com/testing-library/react-testing-library)
---
+++ packages/frontend/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+function Index() {
+  return (
+    <Fragment>
+      <p>Hello, world!</p>
+    </Fragment>
+  );
+}
+
+export default Index;
