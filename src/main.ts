ad2846daa8be5241533b06325d1905856b7de697
nazna <14846330+nazna@users.noreply.github.com>
2021-08-07 20:39:32 +0900

    chore: apply helmet

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { Logger, ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import helmet from 'helmet'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const port = process.env.PORT || 3000
 
   const app = await NestFactory.create(AppModule)
+
+  app.use(helmet())
 
   app.useGlobalPipes(
     new ValidationPipe({
