9a76b196aaf45165729a918e718c4b854c022b88
nazna <14846330+nazna@users.noreply.github.com>
2022-11-04 00:01:55 +0900

    chore: fix unit test mock

--- apps/api-user/src/test/java/dev/nazna/api/user/handler/UserHandlerTest.java
+++ apps/api-user/src/test/java/dev/nazna/api/user/handler/UserHandlerTest.java
@@ -1,16 +1,22 @@
 package dev.nazna.api.user.handler;
 
 import dev.nazna.api.user.domain.model.User;
+import dev.nazna.api.user.usecase.UserUsecase;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
@@ -18,8 +24,9 @@ public class UserHandlerTest {
 
   @Autowired
   RouterFunction<ServerResponse> routerFunction;
-
   WebTestClient client;
+  @MockBean
+  private UserUsecase usecase;
 
   @BeforeEach
   public void setup() {
@@ -28,13 +35,18 @@ public class UserHandlerTest {
 
   @Test
   public void find_ok() {
-    client
+    // arrange
+    when(usecase.find(any())).thenReturn(Mono.just(new User(1L, "alma")));
+
+    // act
+    var actual = client
       .get()
       .uri("/api/users/v1/{id}", "1")
       .accept(MediaType.APPLICATION_JSON)
       .exchange()
-      .expectStatus()
-      .isOk()
+      .expectStatus();
+
+    actual.isOk()
       .expectBody(User.class)
       .isEqualTo(new User(1L, "alma"));
   }
