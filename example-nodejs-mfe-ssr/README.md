cf47555b3870da4ffe5bb17e1906e9518339e347
nazna <14846330+nazna@users.noreply.github.com>
2024-01-14 19:44:36 +0900

    chore: wip, todo replace to experimental repo

--- example-nodejs-mfe-ssr/README.md
+++ example-nodejs-mfe-ssr/README.md
@@ -2,6 +2,11 @@
 
 > Experimental idea of Micro Frontend with Server Side Rendering
 
+## Memo
+
+- 直接 HTML をレスポンスして Hono の HTML Helper で組み込んでも良いが Hono に依存しすぎてしまう
+  - React に乗り換えるのが難しそう
+
 ## References
 
 - [Alpine.js](https://alpinejs.dev/)
--- example-nodejs-mfe-ssr/src/utils.ts
+++ example-nodejs-mfe-ssr/src/utils.ts
@@ -1,6 +1,29 @@
-import { HtmlEscapedString } from 'hono/utils/html';
+import { JSXNode } from 'hono/jsx';
 
-export const serialize = (input: HtmlEscapedString) => {
+const isTextNode = (input: JSXNode) => {
+  return typeof input.tag === 'string' && input.children.every((child) => typeof child === 'string');
+};
+
+const isFunctionNode = (input: JSXNode) => {
+  if (typeof input.tag === 'function') {
+    console.log(input.tag.call(null, { ...input.props, children: input.children }));
+  }
+  return typeof input.tag === 'function';
+};
+
+export const serialize = (input: JSXNode) => {
+  if (isTextNode(input)) {
+    return input;
+  }
+
+  if (isFunctionNode(input)) {
+    serialize(input.tag.call(null, { ...input.props, children: input.children }));
+  }
+
+  for (const child of input.children) {
+    serialize(child);
+  }
+
   return input;
 };
 
